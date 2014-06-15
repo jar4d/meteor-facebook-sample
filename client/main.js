83cf259d836eb84fdf16f80b17b76455b4d30110
Andrew Mager <andrew@mager.co>
2014-06-15 12:57:37 -0700

    FB login works

--- client/main.js
+++ client/main.js
@@ -13,9 +13,7 @@ if (Meteor.isClient) {
 
 Accounts.ui.config({
   requestPermissions: {
-    facebook: ['email', 'user_friends', 'user_location', 'user_events', 
-      'friends_events', 'friends_location', 'friends_about_me',
-      'user_status', 'friends_status', 'read_friendlists'],
+    facebook: ['email', 'user_friends','friends_events', 'friends_location', 'friends_about_me'],
   }
 });
 
